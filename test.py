f2a6cad5d402008884a329f397c73a3d5398a51d
alex <alex.giorev@gmail.com>
2020-10-28 16:05:51 +0200

    moved tests to a single file

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import unittest
-import random
 import string
-import functools
+import random
+import sys
+import os.path
 
 from mre import *
 
@@ -191,5 +192,90 @@ class Test(unittest.TestCase):
         self.assertEqual(
             l, ['a a', 'at aT', 'tHe the', 'LosT loSt', '2009 2009'])
 
-def main():
-    unittest.main(__name__)
+class TestTokenizer(unittest.TestCase):
+    @staticmethod
+    def noflags(regstr):
+        # Just a helper
+        return tokenize(regstr, RegexFlags(0))
+
+    @staticmethod
+    def single(regstr, flags=RegexFlags(0)):
+        # Just a helper
+        return tokenize(regstr, flags)[0]
+    
+    def test_char_class(self):
+        cc = self.single('[abc]')
+        self.assertEqual(cc.data, set('abc'))
+
+        cc = self.single('[a-d]')
+        self.assertEqual(cc.data, set('abcd'))
+
+        cc = self.single('[a-zA-Z0-9]')
+        self.assertEqual(cc.data, set(string.ascii_letters + string.digits))
+        
+        cc = self.single(r'[\x00-\x7f]')
+        self.assertEqual(cc.data, ALL)
+
+        cc = self.single(r'[\d\d]')
+        self.assertEqual(cc.data, set(string.digits))
+
+        cc = self.single(r'[\w\d_-]')
+        self.assertEqual(cc.data, set(string.digits + string.ascii_letters + '_-'))
+
+        # ']' directly after '[' should not end the class.
+        cc = self.single(r'[]abc]')
+        self.assertEqual(cc.data, set(']abc'))
+
+        cc = self.single(r'[^\da-g]')
+        self.assertEqual(cc.data, ALL - (DIGIT | set('abcdefg')))
+
+        # some oddness
+        cc1, cc2, cc3, cc4, cc5 = self.noflags(r'[^-][-][a-c-][*--a][0-5-9]')
+        self.assertEqual(cc1.data, ALL - set('-'))
+        self.assertEqual(cc2.data, set('-'))
+        self.assertEqual(cc3.data, set('abc-'))
+        self.assertEqual(cc4.data, set('*+,-a'))
+        self.assertEqual(cc5.data, set('012345-9'))
+
+    def test_simple(self):
+        types = ['^', '$', r'\A', r'\b', r'\B', r'\Z', '|', '(', '(?:',
+                 '(?=','(?!', ')']
+        random.shuffle(types) # order shouldn't matter.
+        together = ''.join(types)
+        result_types = [token.type for token in self.noflags(together)]
+        self.assertEqual(types, result_types)
+
+    def test_greedy_quant(self):
+        quants = ['+', '?', '*', '{10,20}']
+        tokens = self.noflags('+?*{10,20}')
+        expected_bounds = [(1, None), (0, 1), (0, None), (10, 20)]
+        for token, eb in zip(tokens, expected_bounds):
+            self.assertEqual(token.type, 'greedy-quant')
+            self.assertEqual(token.data, eb)
+
+        expected_bounds = set(expected_bounds) # order no longer matters
+        for k in range(24):
+            random.shuffle(quants)
+            tokens = self.noflags(''.join(quants))
+            bounds = {token.data for token in tokens}
+            self.assertEqual(bounds, expected_bounds)
+
+class TestParser(unittest.TestCase):
+    @staticmethod
+    def nf(regstr):
+        """Parse without flags."""
+        return parse(regstr, RegexFlags(0))
+
+    def test1(self):
+        p = compile("^The")
+        m = p.match("The table is clean.")
+        self.assertTrue(m)
+        self.assertTrue(m.group(0), "The")
+
+    def test2(self):
+        p = compile("abc|xyz", IGNORECASE)
+        lst = p.findall('--aBc-_.h--XyZ--ABC-abcxYZ-')
+        expected = ['aBc', 'XyZ', 'ABC', 'abc', 'xYZ']
+        self.assertEqual(expected, lst)
+
+unittest.main(__name__)
--- tests/run.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os.path
-
-sys.path[0] = os.path.dirname(os.getcwd())
-
-from tests import test_patterns, test_parser
-
-test_patterns.main()
-test_parser.main()
